3c287e9ab26796593425659e7c4b2699e66857d6
martinezjm1968 <121067795+martinezjm1968@users.noreply.github.com>
2023-04-03 14:43:06 -0300

    Trabajo final

--- js/buscar.js
+++ js/buscar.js
@@ -1,70 +1,63 @@
-try {
-    // La busqueda se realiza sobre el campo modelo por lo que debo ingresar el mismo nombre que se encuentra en localStorage
 
-    // Leo el estado del boton buscar
-    let boton = document.getElementById("botonBuscar")
-    boton.addEventListener("click", respuestaClick)
+// La busqueda se realiza sobre el campo modelo por lo que debo ingresar el mismo 
+// nombre que se encuentra en localStorage / api
+
+// Leo el estado del boton buscar
+let boton = document.getElementById("botonBuscar")
+// Traigo lo que pongo en el input buscar en html
+let aBuscar = document.getElementById("buscar")
+console.log("Modelo a Buscar: " + aBuscar.value)
+// Traigo la estructura del "nodo" del HTML con el ID = "productos" que es un DIV
+const busquedaResult = document.getElementById('busquedaResult');
+
+boton.addEventListener("click", respuestaClick)
+
+function respuestaClick() {
+    // Esto lo hago desde el localStorage
+    //let roster = JSON.parse(localStorage.getItem("roster")) || []
+    //console.log(roster);
+
+    // Esto lo hago desde una API, en este caso local!
+    fetch('../api/api_roster.json')
+        .then(response => response.json())
+        .then(roster => encontrado(roster))
+}
 
 
-    function respuestaClick() {
 
-        // Traigo lo que pongo en el input buscar en html
-        let aBuscar = document.getElementById("buscar")
-        console.log("Modelo a Buscar: " + aBuscar.value)
+// Hago la búsqueda
+function encontrado(roster) {
+    let encontrado = roster.find(objeto => objeto.modelo.toLowerCase() == aBuscar.value.toLowerCase());
+    //console.log('Resultado encontrado:  + ${JSON.stringfy(encontrado)}')
 
-        let roster = JSON.parse(localStorage.getItem("roster")) || []
-
-        console.log(roster);
-
-        // Hago la búsqueda
-        let encontrado = roster.filter(objeto => objeto.modelo.toLowerCase() == aBuscar.value.toLowerCase());
-        console.log("Resultado encontrado: " + encontrado);
-
-        if (encontrado) {
-            // Me fijo si el objeto "encontrado" esta vacío. 
-            if (Object.entries(encontrado).length > 0) {
-
-
-                // Traigo la estructura del "nodo" del HTML con el ID = "productos" que es un DIV
-                const busquedaResult = document.getElementById('busquedaResult');
-
-                // Recorro el objeto para presentar los resultados en HTML con el innerHTML
-                encontrado.forEach(el => {
-
-                    busquedaResult.innerHTML +=
-                        `
+    if (encontrado) {
+        // Me fijo si el objeto "encontrado" esta vacío. 
+        busquedaResult.innerHTML +=
+            `
                 <div class="col">
                     <div class="card h-40"> 
-                        <img src="${el.imagen}" class="card-img-top" alt="...">
+                        <img src="${encontrado.imagen}" class="card-img-top" alt="...">
                         <div class="card-body">
-                            <h5 class="card-title">${el.modelo}</h5>
-                            <p class="card-text">Tipo: ${el.tipo}</p>
-                            <p class="card-text">Nro locomotora: ${el.nro}</p>
-                            <p class="card-text">Nro locomotora: ${el.marca}</p>
-                            <p class="card-text">Fecha Compra: ${el.fechaCompra}</p>
-                            <p class="card-text">Sistema: ${el.dcc}</p>
-                            <p class="card-text">Dir DCC: ${el.dir_dcc}</p>
-                            <p class="card-text">Compañia: ${el.cia}</p>
-                            <p class="card-text">Costo: u$${el.costo}</p>
-                            <p class="card-text">Precio: u$${el.precio}</p>
+                            <h5 class="card-title">${encontrado.modelo}</h5>
+                            <p class="card-text">Tipo: ${encontrado.tipo}</p>
+                            <p class="card-text">Nro locomotora: ${encontrado.nro}</p>
+                            <p class="card-text">Nro locomotora: ${encontrado.marca}</p>
+                            <p class="card-text">Fecha Compra: ${encontrado.fechaCompra}</p>
+                            <p class="card-text">Sistema: ${encontrado.dcc}</p>
+                            <p class="card-text">Dir DCC: ${encontrado.dir_dcc}</p>
+                            <p class="card-text">Compañia: ${encontrado.cia}</p>
+                            <p class="card-text">Costo: u$${encontrado.costo}</p>
+                            <p class="card-text">Precio: u$${encontrado.precio}</p>
                         </div>
                     </div>
                 </div>
                 `
-                })
-
-            } else {
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Oops...',
-                    text: 'No se pudo encontrar!',
-                    //footer: '<a href="">Why do I have this issue?</a>'
-                })
-            }}
-        }
-    } catch (err) {
-        alert(`Error detectado:
-     ${err.name}
-     ${err.message} 
-     ${err.stack}`);
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'No se pudo encontrar!',
+            //footer: '<a href="">Why do I have this issue?</a>'
+        })
     }
+}
--- js/listarLocos.js
+++ js/listarLocos.js
@@ -1,114 +1,113 @@
-try {
-    let roster = JSON.parse(localStorage.getItem("roster")) || []
 
-    console.log("Storage: " + roster);
+// let roster = JSON.parse(localStorage.getItem("roster")) || []
 
-    const form = document.getElementById("formulario");
+function traerRoster() {
+    // Esto lo hago desde una API, en este caso local!
+    fetch('../api/api_roster.json')
+        .then(response => response.json())
+        .then(roster => roster)
+}
 
-    // Traigo la estructura del "nodo" del HTML con el ID = "productos" que es un DIV
-    const locomotoras = document.getElementById('locomotoras');
-    let contador = 0;
-    // Recorro el objeto para presentar los resultados en HTML con el innerHTML
-    roster.forEach(el => {
+let roster = traerRoster()
+const form = document.getElementById("formulario");
 
-        locomotoras.innerHTML +=
-            `
+// Traigo la estructura del "nodo" del HTML con el ID = "productos" que es un DIV
+const locomotoras = document.getElementById('locomotoras');
+let contador = 0;
+// Recorro el objeto para presentar los resultados en HTML con el innerHTML
+//roster.forEach(el => {
+
+    locomotoras.innerHTML +=
+        `
         <div class="col" id="card-${contador}">
             <div class="card h-40"> 
-                <img src="${el.imagen}" class="card-img-top" alt="...">
+                <img src="${roster.imagen}" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">${el.modelo}</h5>
-                    <p class="card-text">Tipo: ${el.tipo}</p>
-                    <p class="card-text">Nro locomotora: ${el.nro}</p>
-                    <p class="card-text">Marca: ${el.marca}</p>
-                    <p class="card-text">Fecha Compra: ${el.fechaCompra}</p>
-                    <p class="card-text">Sistema: ${el.dcc}</p>
-                    <p class="card-text">Dir DCC: ${el.dir_dcc}</p>
-                    <p class="card-text">Compañia: ${el.cia}</p>
-                    <p class="card-text">Costo: u$${el.costo}</p>
-                    <p class="card-text">Precio: u$${el.precio}</p>
+                    <h5 class="card-title">${roster.modelo}</h5>
+                    <p class="card-text">Tipo: ${roster.tipo}</p>
+                    <p class="card-text">Nro locomotora: ${roster.nro}</p>
+                    <p class="card-text">Marca: ${roster.marca}</p>
+                    <p class="card-text">Fecha Compra: ${roster.fechaCompra}</p>
+                    <p class="card-text">Sistema: ${roster.dcc}</p>
+                    <p class="card-text">Dir DCC: ${roster.dir_dcc}</p>
+                    <p class="card-text">Compañia: ${roster.cia}</p>
+                    <p class="card-text">Costo: u$${roster.costo}</p>
+                    <p class="card-text">Precio: u$${roster.precio}</p>
 
                     <a href="#" class="btn btn-primary" onclick="borrarCard(${contador})">Borrar Card</a>
                 </div>
             </div>
         </div>
         `
-        contador++;
+    contador++;
+//})
+
+// Funcion borrarCard
+function borrarCard(id) {
+    console.log(id);
+
+    let objetoModificado = roster.find(objeto => objeto.id === id);
+
+    // SweetAlert
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+            confirmButton: 'btn btn-success',
+            cancelButton: 'btn btn-danger'
+        },
+        buttonsStyling: false
     })
 
-    // Funcion borrarCard
-    function borrarCard(id) {
-        console.log(id);
+    swalWithBootstrapButtons.fire({
+        title: 'Estas Seguro?',
+        text: "No podras deshacerlo!",
+        icon: 'Cuidado',
+        showCancelButton: true,
+        confirmButtonText: 'Si, borrarlo!',
+        cancelButtonText: 'No, cancelar!',
+        reverseButtons: true
+    }).then((result) => {
+        if (result.isConfirmed) {
+            let borrado = roster.splice(id, 1)
 
-        let objetoModificado = roster.find(objeto => objeto.id === id);
-
-        // SweetAlert
-        const swalWithBootstrapButtons = Swal.mixin({
-            customClass: {
-                confirmButton: 'btn btn-success',
-                cancelButton: 'btn btn-danger'
-            },
-            buttonsStyling: false
-        })
-
-        swalWithBootstrapButtons.fire({
-            title: 'Estas Seguro?',
-            text: "No podras deshacerlo!",
-            icon: 'Cuidado',
-            showCancelButton: true,
-            confirmButtonText: 'Si, borrarlo!',
-            cancelButtonText: 'No, cancelar!',
-            reverseButtons: true
-        }).then((result) => {
-            if (result.isConfirmed) {
-                let borrado = roster.splice(id, 1)
-
-                // // Con este contador regenero los índices según el length.
-                let i = 0
-                roster.forEach(el => {
-                    el.id = i
-                    i++
-                })
+            // // Con este contador regenero los índices según el length.
+            let i = 0
+            roster.forEach(el => {
+                el.id = i
+                i++
+            })
 
 
-                const rosterJSON = JSON.stringify(roster)
+            const rosterJSON = JSON.stringify(roster)
 
-                localStorage.setItem("roster", rosterJSON)
+            localStorage.setItem("roster", rosterJSON)
 
-                console.log(roster)
-                swalWithBootstrapButtons.fire(
-                    'Borrado!',
-                    'Su Card fue borrada',
-                    'exitosamente'
-                )
-                setTimeout(() => {
-                    location.reload()
-                }, 3000);
-                
-
-            } else if (
-                /* Read more about handling dismissals below */
-                result.dismiss === Swal.DismissReason.cancel
-            ) {
-                swalWithBootstrapButtons.fire(
-                    'Cancelado',
-                    'Su Card no fue modificada :)',
-                    'error'
-                )
-            }
-        })
-
-        // Esta funcion refresca la pantalla para borrar lo que no quiero dejar
-        
-        //    window.location.href = window.location.href;
-        
-
-    }
+            console.log(roster)
+            swalWithBootstrapButtons.fire(
+                'Borrado!',
+                'Su Card fue borrada',
+                'exitosamente'
+            )
+            setTimeout(() => {
+                location.reload()
+            }, 3000);
 
 
-} catch (err) {
-    alert(`Error detectado:
-     ${err.name}
-     ${err.message} 
-     ${err.stack}`);
+        } else if (
+            /* Read more about handling dismissals below */
+            result.dismiss === Swal.DismissReason.cancel
+        ) {
+            swalWithBootstrapButtons.fire(
+                'Cancelado',
+                'Su Card no fue modificada :)',
+                'error'
+            )
+        }
+    })
+
+    // Esta funcion refresca la pantalla para borrar lo que no quiero dejar
+
+    //    window.location.href = window.location.href;
+
+
 }
+
